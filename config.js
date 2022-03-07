a4e8b07f866b600f1905419d4c2f4c1252b122cb
dsfahbfhjsa22 <101074644+dsfahbfhjsa22@users.noreply.github.com>
2022-03-07 16:52:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -12,5 +12,5 @@ const CONFIG = {
     mess: 'chúng tớ biết mà, haizzz tiếc thật 😞 nếu gặp cậu sớm hơn thì bài văn tả thiên thần của tớ sẽ không phải giấy trắng 🤭',
     messDesc: 'Mùng 8/3 chúc cậu luôn vui vẻ, mạnh khỏe, chúng tớ sẽ mãi bên cậu 😍😍😍  chuẩn bị nhìn thấy thiên thần lớp 11a5 nè',
     btnAccept: 'tớ cảm động rớt nước mắt đó, cảm ơn nhaaa <33333',
-    messLink: 'https://l.facebook.com/l.php?u=https%3A%2F%2Fpadlet.com%2Fnguyentlovie126%2F55l5ehha2qbqq860%3Ffbclid%3DIwAR2AXFEsi3BJGu-I9Ho7IPcvwRiKwoOq2r_AuVRuTEiZ4PC2mCv2mH9WNmU&h=AT3bfOxetdHMlNzTVlM304WCalwZ-0oQ_ILID_B7aIOuTb6i1fftPmmKmKwB8ETfLkS5Ivh-FvzoSNisM4THTq2jVuZQN8GFcyYtDQP6L9sJCVXjrA31wexL4F4ws6I1atx6J7MN3gnHRTb8nW9caw'
+    messLink: 'https://l.php?u=https%3A%2F%2Fpadlet.com%2Fnguyentlovie126%2F55l5ehha2qbqq860%3Ffbclid%3DIwAR2AXFEsi3BJGu-I9Ho7IPcvwRiKwoOq2r_AuVRuTEiZ4PC2mCv2mH9WNmU&h=AT3bfOxetdHMlNzTVlM304WCalwZ-0oQ_ILID_B7aIOuTb6i1fftPmmKmKwB8ETfLkS5Ivh-FvzoSNisM4THTq2jVuZQN8GFcyYtDQP6L9sJCVXjrA31wexL4F4ws6I1atx6J7MN3gnHRTb8nW9caw'
 }
