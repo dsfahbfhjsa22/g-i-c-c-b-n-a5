ac92b411adb2cd5784a2b8d5ff92f6e069f849ee
dsfahbfhjsa22 <101074644+dsfahbfhjsa22@users.noreply.github.com>
2022-03-07 17:01:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -12,5 +12,5 @@ const CONFIG = {
     mess: 'chúng tớ biết mà, haizzz tiếc thật 😞 nếu gặp cậu sớm hơn thì bài văn tả thiên thần của tớ sẽ không phải giấy trắng 🤭',
     messDesc: 'Mùng 8/3 chúc cậu luôn vui vẻ, mạnh khỏe, chúng tớ sẽ mãi bên cậu 😍😍😍  chuẩn bị nhìn thấy thiên thần lớp 11a5 nè',
     btnAccept: 'tớ cảm động rớt nước mắt đó, cảm ơn nhaaa <33333',
-    messLink: 'https://l.facebook.com/l.php?u=https%3A%2F%2Fpadlet.com%2Fnguyentlovie126%2F55l5ehha2qbqq860%3Ffbclid%3DIwAR0BnNanN-3Bfn1X-BpjYeeF3IB_KHxrnTHGWQ6OQsEFWnJD3RVb9jpAB28&h=AT1iCd0eqO4_7XdK90B2TfYF_3CukztHL2DMhQilDY-Uu7KRUKHWgy0vukh1wfLL4vRnoqV09-LSi-0xF0BYBe76MFB18WEshh-hyFxOnrNRZlhBkrjn3-3IKNwAu2Y5Pkw9OdH4w5k1i_EPbjDBKg'
+    messLink: 'https://padlet.com/nguyentlovie126/55l5ehha2qbqq860'
 }
